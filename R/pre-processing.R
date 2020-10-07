be71aec203a2dcf496ee6c302d2dabb101bb7ca5
Fernando Cagua <efcaguab@gmail.com>
2020-10-07 22:07:35 +1300

    Use function to check for unique rows for ids

--- R/pre-processing.R
+++ R/pre-processing.R
@@ -45,3 +45,11 @@ make_id <- function(x, prefix){
                           width = floor(log10(max(x))) + 1,
                           pad = "0"))
 }
+
+# check whether there is a single row for each value in column_name
+has_single_row <- function(df, column_name){
+  df %>%
+    dplyr::count({{column_name}}) %$%
+    magrittr::is_greater_than(n, 1) %>%
+    any()
+}
--- R/species-info.R
+++ R/species-info.R
@@ -20,19 +20,15 @@ get_species_info <- function(fish_data_raw, species_to_exclude){
 
 # Test that the species info frame is fine
 test_species_info <- function(species_info){
+
   multiple_rows_per_sp_code <- species_info %>%
-    dplyr::count(species) %$%
-    magrittr::is_greater_than(n, 1) %>%
-    any()
+    has_single_row(species)
 
   if (multiple_rows_per_sp_code)
     stop("Multiple rows per species code in species_info table")
 
   multiple_rows_per_sp_name <- species_info %>%
-    dplyr::mutate(species_name = tolower(species_name)) %>%
-    dplyr::count(species_code) %$%
-    magrittr::is_greater_than(n, 1) %>%
-    any()
+    has_single_row(species_name)
 
   if (multiple_rows_per_sp_name)
     stop("Multiple rows per species name in species_info table")
